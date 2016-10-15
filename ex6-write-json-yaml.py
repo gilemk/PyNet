e802561c5e2c68ffacc0df40266498b4a5731ae3
Igor Donev <igor.donev@gmail.com>
2016-10-15 01:34:29 -0700

    Exercise 6

---
+++ ex6-write-json-yaml.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+import json
+import yaml
+
+def main():
+    my_list = range(10)
+    my_list.append('Router')
+    my_list.append({})
+    my_list[-1]['ip address'] = '10.10.10.10'
+    my_list[-1]['vendors'] = ['juniper', 'cisco', 'arista']
+    my_list.append('Switch')
+
+    with open('test.yml', 'w') as f:
+        f.write(yaml.dump(my_list, default_flow_style=False))
+
+    with open('test.json', 'w') as f:
+        json.dump(my_list, f)
+
+if __name__ == "__main__":
+    main()
+
+          
